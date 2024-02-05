9a6b8390ea6a2b4834e3d70b56f79879a67d150b
Bargamotova <anna.samoilenko.2012@gmail.com>
2024-02-05 19:57:46 +0900

    updated

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -99,6 +99,7 @@ export const subscribe = async (req, res, next) => {
     res.status(200).json('Subscriptions successful');
   } catch (error) {
     next(error);
+    console.log(error)
   }
 }
 export const unsubscribe = async (req, res, next) => {
