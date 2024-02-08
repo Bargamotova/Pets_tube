47e4ecb900034b84bc30d699724d27a8f405383b
Bargamotova <anna.samoilenko.2012@gmail.com>
2024-02-08 14:16:43 +0900

    update access token

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,20 +5,20 @@ import { verifyAccessToken, verifyToken } from '../utils/verifyToken.js';
 const router = express.Router();
 
 // update user 
-router.put('/:id', verifyToken, update)
-router.post("/change-avatar", verifyToken, changeAvatar)
+router.put('/:id', verifyAccessToken, update)
+router.post("/change-avatar", verifyAccessToken, changeAvatar)
 // delete user 
-router.delete('/:id', verifyToken, deleteUser)
+router.delete('/:id', verifyAccessToken, deleteUser)
 // get a user 
 router.get('/find/:id', getUser)
 // subscribe a user 
-router.put('/sub/:id', verifyAccessToken, subscribe)
+router.put('/sub/:id', subscribe)
 // unsubscribe a user 
-router.put('/unsub/:id', verifyAccessToken, unsubscribe)
+router.put('/unsub/:id', unsubscribe)
 // like a video 
-router.put('/like/:videoId', verifyAccessToken, like)
+router.put('/like/:videoId', like)
 // dislike a video
-router.put('/dislike/:videoId', verifyAccessToken, dislike)
+router.put('/dislike/:videoId', dislike)
 
 
 
